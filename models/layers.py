04fbc1d76fa4a4f650e34bdf43be3a0ff9012bd0
yangsenius <465097485@qq.com>
2018-08-16 10:57:34 +0800

    12

---
+++ models/layers.py
@@ -0,0 +1,227 @@
+from torch import nn
+from torch.autograd import Function
+import sys 
+import time
+import numpy as np
+import cv2
+import torch
+import torchvision.models
+
+Pool = nn.MaxPool2d
+
+def batchnorm(x):
+    return nn.BatchNorm2d(x.size()[1])(x)
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        n = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
+        m.weight.data.normal_(0, (1./n)**0.5) 
+
+class Full(nn.Module):
+    def __init__(self, inp_dim, out_dim, bn = False, relu = False):
+        super(Full, self).__init__()
+        self.fc = nn.Linear(inp_dim, out_dim, bias = True)
+        self.relu = None
+        self.bn = None
+        if relu:
+            self.relu = nn.ReLU()
+        if bn:
+            self.bn = nn.BatchNorm2d(out_dim)
+
+    def forward(self, x):
+        x = self.fc(x.view(x.size()[0], -1))
+        if self.relu is not None:
+            x = self.relu(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        return x
+
+class Conv(nn.Module):
+    def __init__(self, inp_dim, out_dim, kernel_size=3, stride = 1, bn = False, relu = True):
+        super(Conv, self).__init__()
+        self.inp_dim = inp_dim
+        self.conv = nn.Conv2d(inp_dim, out_dim, kernel_size, stride, padding=(kernel_size-1)//2, bias=True)
+        self.relu = None
+        self.bn = None
+        if relu:
+            self.relu = nn.ReLU()
+        if bn:
+            self.bn = nn.BatchNorm2d(out_dim)
+
+    def forward(self, x):
+        assert x.size()[1] == self.inp_dim, "{} {}".format(x.size()[1], self.inp_dim)
+        x = self.conv(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        return x
+
+class Hourglass(nn.Module):
+    def __init__(self, n, f, bn=None, increase=128):
+        super(Hourglass, self).__init__()
+        nf = f + increase
+        self.up1 = Conv(f, f, 3, bn=bn)
+        # Lower branch
+        self.pool1 = Pool(2, 2)
+        self.low1 = Conv(f, nf, 3, bn=bn)
+        # Recursive hourglass
+        if n > 1:
+            self.low2 = Hourglass(n-1, nf, bn=bn)
+        else:
+            self.low2 = Conv(nf, nf, 3, bn=bn)
+        self.low3 = Conv(nf, f, 3)
+        self.up2  = nn.UpsamplingNearest2d(scale_factor=2)
+
+    def forward(self, x):
+        up1  = self.up1(x)
+        pool1 = self.pool1(x)
+        low1 = self.low1(pool1)
+        low2 = self.low2(low1)
+        low3 = self.low3(low2)
+        up2  = self.up2(low3)
+        return up1 + up2
+
+
+"""
+coordconv
+"""
+class AddCoordsTh(nn.Module):
+    def __init__(self, x_dim=64, y_dim=64, with_r=False):
+        super(AddCoordsTh, self).__init__()
+        self.x_dim = x_dim
+        self.y_dim = y_dim
+        self.with_r = with_r
+
+    def forward(self, input_tensor):
+        """
+        input_tensor: (batch, c, x_dim, y_dim)
+        """
+        batch_size_tensor = input_tensor.shape[0]
+
+        xx_ones = torch.ones([1, self.y_dim], dtype=torch.int32)
+        xx_ones = xx_ones.unsqueeze(-1)
+
+        xx_range = torch.arange(self.x_dim, dtype=torch.int32).unsqueeze(0)
+        xx_range = xx_range.unsqueeze(1)
+
+        xx_channel = torch.matmul(xx_ones, xx_range)
+        xx_channel = xx_channel.unsqueeze(-1)
+
+        yy_ones = torch.ones([1, self.x_dim], dtype=torch.int32)
+        yy_ones = yy_ones.unsqueeze(1)
+
+        yy_range = torch.arange(self.y_dim, dtype=torch.int32).unsqueeze(0)
+        yy_range = yy_range.unsqueeze(-1)
+
+        yy_channel = torch.matmul(yy_range, yy_ones)
+        yy_channel = yy_channel.unsqueeze(-1)
+
+        xx_channel = xx_channel.permute(0, 3, 2, 1)
+        yy_channel = yy_channel.permute(0, 3, 2, 1)
+
+        xx_channel = xx_channel.float() / (self.x_dim - 1)
+        yy_channel = yy_channel.float() / (self.y_dim - 1)
+
+        xx_channel = xx_channel * 2 - 1
+        yy_channel = yy_channel * 2 - 1
+
+        xx_channel = xx_channel.repeat(batch_size_tensor, 1, 1, 1)
+        yy_channel = yy_channel.repeat(batch_size_tensor, 1, 1, 1)
+        print ("xx:{}".format(xx_channel))
+        print ("yy:{}".format(yy_channel))
+        print ("input:{}".format(input_tensor))
+        ret = torch.cat([input_tensor, xx_channel, yy_channel], dim=1)
+
+        if self.with_r:
+            rr = torch.sqrt(torch.pow(xx_channel - 0.5, 2) + torch.pow(yy_channel - 0.5, 2))
+            ret = torch.cat([ret, rr], dim=1)
+
+        return ret
+
+
+class CoordConvTh(nn.Module):
+    """CoordConv layer as in the paper."""
+    def __init__(self, x_dim, y_dim, with_r, kernel_size=3, stride = 1, bn = False, relu = True):
+        super(CoordConvTh, self).__init__()
+        self.addcoords = AddCoordsTh(x_dim=x_dim, y_dim=y_dim, with_r=with_r)
+        self.conv = nn.Conv2d(x_dim+2, y_dim, kernel_size, stride, padding=(kernel_size-1)//2, bias=True)
+        self.relu = None
+        self.bn = None
+        if relu:
+            self.relu = nn.ReLU()
+        if bn:
+            self.bn = nn.BatchNorm2d(y_dim)
+
+    def forward(self, input_tensor):
+        ret = self.addcoords(input_tensor)
+        ret = self.conv(ret)
+        if self.relu is not None:
+                ret = self.relu(ret)
+        if self.bn is not None:
+            ret = self.bn(ret)
+        return ret
+
+
+'''
+An alternative implementation for PyTorch with auto-infering the x-y dimensions.
+'''
+class AddCoords(nn.Module):
+
+    def __init__(self, with_r=False):
+        super().__init__()
+        self.with_r = with_r
+
+    def forward(self, input_tensor):
+        """
+        Args:
+            input_tensor: shape(batch, channel, x_dim, y_dim)
+        """
+        batch_size, _, x_dim, y_dim = input_tensor.size()
+
+        xx_channel = torch.arange(x_dim).repeat(1, y_dim, 1)
+        yy_channel = torch.arange(y_dim).repeat(1, x_dim, 1).transpose(1, 2)
+
+        xx_channel = xx_channel.float() / (x_dim - 1)
+        yy_channel = yy_channel.float() / (y_dim - 1)
+
+        xx_channel = xx_channel * 2 - 1
+        yy_channel = yy_channel * 2 - 1
+
+        xx_channel = xx_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
+        yy_channel = yy_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
+
+        ret = torch.cat([
+            input_tensor,
+            xx_channel.type_as(input_tensor),
+            yy_channel.type_as(input_tensor)], dim=1)
+
+        if self.with_r:
+            rr = torch.sqrt(torch.pow(xx_channel - 0.5, 2) + torch.pow(yy_channel - 0.5, 2))
+            ret = torch.cat([ret, rr], dim=1)
+
+        return ret
+
+
+class CoordConv(nn.Module):
+    
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride = 1, bn = False, relu = True, with_r=False, **kwargs):
+        super().__init__()
+        self.addcoords = AddCoords(with_r=with_r)
+        self.conv = nn.Conv2d(in_channels + 2, out_channels, kernel_size, stride, padding=(kernel_size-1)//2, bias=True, **kwargs)
+        self.relu = None
+        self.bn = None
+        if relu:
+            self.relu = nn.ReLU()
+        if bn:
+            self.bn = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        ret = self.addcoords(x)
+        ret = self.conv(ret)
+        if self.relu is not None:
+            ret = self.relu(ret)
+        if self.bn is not None:
+            ret = self.bn(ret)
+        return ret
